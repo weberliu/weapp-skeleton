c8331b6d96aa8e205f0600c4f03b191f0177f948
Weber Liu <weber.liu@gmail.com>
2018-10-31 08:45:11 +0800

    add iphone x checker

--- src/app.js
+++ src/app.js
@@ -2,25 +2,30 @@
  * @fileOverview 微信小程序的入口文件
  */
 
-import { wx, qcloud } from './vendor/weapp-promise/index'
-import config from './config/index'
+import config from './config'
+import promisfy from './lib/promisfy'
 
 App({
+  globalData: {
+    isIpx: false
+  },
+
   /**
    * 小程序初始化时执行，我们初始化客户端的登录地址，以支持所有的会话操作
    */
   onLaunch () {
-    qcloud.setLoginUrl(config.service.loginUrl)
-
-    wx.getNetworkType()
-      .then(res => {
-        if (res.networkType === 'none') {
-          return wx.showMessage('提示', '请检查您当前的网络状态')
-        }
-      })
+    this.checkIsIPhoneX()
   },
 
-  globalData: {
-    hasLogin: false
+  checkIsIPhoneX () {
+    const self = this
+    wx.getSystemInfo({
+      success: function (res) {
+        // 根据 model 进行判断
+        if (res.model.search('iPhone X') !== -1) {
+          self.globalData.isIPX = true
+        }
+      }
+    })
   }
 })
